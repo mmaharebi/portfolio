fb88b92a1d436da505a9c4b53f936549ccc2b665
Mahdi <mmaharebi@gmail.com>
2025-11-09 13:56:18 +0100

    bug fix: nav buttons scroll behavior and nextjs link behavior.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -53,6 +54,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ScrollToTop />
         <Header />
         <main className="min-h-screen">
           {children}
